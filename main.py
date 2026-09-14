02637ac5aec77b438ecc3f6fad5370aa33302031
Mohamedweli <mohamedwelihassan80@gmail.com>
2026-09-14 12:35:28 +0300

    another commit

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ age = 23
 favourite_language = "python"
 goal = "i want to learn python programming language"
 
-print("Hello, my name is", name)
-print("I am", age, "years old")
-print("My favourite programming language is", favourite_language)
-print("My goal for this course is:", goal)
+print(f"Hello, my name is {name}")
+print(f"I am {age} years old")
+print(f"My favourite programming language is {favourite_language}")
+print(f"My goal for this course is: {goal}")
