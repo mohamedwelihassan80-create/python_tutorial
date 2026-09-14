6a8944aeaf6ee4db8fa517465bac3998481710ba
Mohamedweli <mohamedwelihassan80@gmail.com>
2026-09-14 11:51:38 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,13 @@
+# Storing variable name and age
+name = "Mohamedweli" 
+age = 23  
+
+
+# writing languages and goals for this course
+favourite_language = "python"
+goal = "i want to learn python programming language"
+
+print(name)
+print(age)
+print(favourite_language)
+print(goal)
